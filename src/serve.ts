8261876107c2b2a09c8f6292eb372675f874ff4a
maria heloisa <maria.pizarro@bancointer.com.br>
2022-12-22 21:21:25 -0300

    mac3

--- src/serve.ts
+++ src/serve.ts
@@ -1,10 +1,12 @@
-import express from 'express'
+import express   from 'express'
 import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 
-app.use(express.json()
+app.use(express.json())
 
-app.use(categoriesRoutes))
+app.use(categoriesRoutes)
+
+
 
 app.listen(3333, () => console.log('Server is running'))
